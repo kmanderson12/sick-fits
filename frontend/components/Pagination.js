7f66b2d3dd726a941335d684bb401069c57d7d23
Kyle Anderson <kmanderson12@gmail.com>
2019-08-08 21:18:12 -0500

    pagination tests

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,10 +1,10 @@
-import React from "react";
-import PaginationStyles from "./styles/PaginationStyles";
-import Head from "next/head";
-import Link from "next/link";
-import gql from "graphql-tag";
-import { Query } from "react-apollo";
-import { perPage } from "../config";
+import React from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+import Head from 'next/head';
+import Link from 'next/link';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { perPage } from '../config';
 
 const PAGINATION_QUERY = gql`
   query PAGINATION_QUERY {
@@ -24,7 +24,7 @@ const Pagination = props => (
       const pages = Math.ceil(count / perPage);
       const page = props.page;
       return (
-        <PaginationStyles>
+        <PaginationStyles data-test="pagination">
           <Head>
             <title>
               Sick Fits! - Page {page} of {pages}
@@ -33,22 +33,28 @@ const Pagination = props => (
           <Link
             prefetch
             href={{
-              pathname: "items",
+              pathname: 'items',
               query: { page: page - 1 }
             }}
           >
-            <a className="prev" aria-disabled={page <= 1}>← Prev</a>
+            <a className="prev" aria-disabled={page <= 1}>
+              ← Prev
+            </a>
           </Link>
-          <p>Page {page} of {pages}</p>
+          <p>
+            Page {page} of <span className="totalPages">{pages}</span>
+          </p>
           <p>{count} Items Total</p>
           <Link
             prefetch
             href={{
-              pathname: "items",
+              pathname: 'items',
               query: { page: page + 1 }
             }}
           >
-            <a className="next" aria-disabled={page >= pages}>Next →</a>
+            <a className="next" aria-disabled={page >= pages}>
+              Next →
+            </a>
           </Link>
         </PaginationStyles>
       );
@@ -57,3 +63,4 @@ const Pagination = props => (
 );
 
 export default Pagination;
+export { PAGINATION_QUERY };
